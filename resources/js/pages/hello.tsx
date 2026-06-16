e01c1cd01c9f9940e738bd43fbf110bedc4649bc
vachan-maker <vachan@tutanota.com>
2026-06-16 09:50:41 +0530

    Started project

---
+++ resources/js/pages/hello.tsx
@@ -0,0 +1,8 @@
+
+import { Layout } from "lucide-react";
+export default function Hello() {
+    return (
+        <h1>Welcome. This is my first react application built with laravel</h1>
+    );
+
+}
