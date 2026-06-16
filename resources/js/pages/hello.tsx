fc5de2c83fe9ffb14def776c770a82ea207254fe
vachan-maker <vachan@tutanota.com>
2026-06-16 10:08:37 +0530

    Fetching images from fakestore

--- resources/js/pages/hello.tsx
+++ resources/js/pages/hello.tsx
@@ -1,8 +1,43 @@
 
 import { Layout } from "lucide-react";
+import { useState } from "react";
+import { useEffect } from "react";
+type Product = {
+    id: number;
+    title:string,
+    price:number,
+    image:string
+
+};
 export default function Hello() {
+    const [products, setProducts] = useState<Product[]>([]);
+    useEffect(()=>{
+        const fetchProducts = async () => {
+            try {
+                const response = (await fetch('https://fakestoreapi.com/products'))
+                const data = await response.json();
+                setProducts(data);
+                if(!response.ok) {
+                    throw new Error("Failed to fetch Products");
+                }
+            }
+
+            catch(err) {
+                console.log(err);
+            }
+        }
+        fetchProducts();
+    },[])
     return (
-        <h1>Welcome. This is my first react application built with laravel</h1>
+        <>
+        {products?.map((product) =>(
+            <div key={product.id}>
+                <img src={product.image}/>
+                <p>{product.title}</p>
+                <p>{product.price}</p>
+            </div>
+        ))}
+        </>
     );
 
 }
